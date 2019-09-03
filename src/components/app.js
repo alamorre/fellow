04588d738d51feaaac4f43039d86d914e4dc734b
Adam La Morre <alamorre@gmail.com>
2019-09-03 10:43:58 -0400

    Ready to make get started page

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
+// Handle the urls and browser history
+import { Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
+
 import { Grid } from './board'
 
 export default class App extends Component {
   render() {
     return (
-      <Grid />
+      <Router history={createBrowserHistory()}>
+        <Switch>
+          <Route path='/:id' component={Grid} />
+          <Route path='/' component={Grid} />
+        </Switch>
+      </Router>
     );
   }
 }
