fc443ba4edbd21a05e3cf0daad59854430a9ad29
Adam La Morre <alamorre@gmail.com>
2019-09-03 08:39:06 -0400

    first commit (antd applied)

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { Button } from 'antd'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Button>React simple starter</Button>
+    );
+  }
+}
