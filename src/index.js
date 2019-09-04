43e9230583712b89aca4f2df6f873c343ced0689
Adam La Morre <alamorre@gmail.com>
2019-09-04 19:59:37 -0400

    Commented index js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-import App from './components/app';
-import reducers from './reducers';
+import App from './components/app'
+import reducers from './reducers'
 
-import thunk from "redux-thunk";
-import promise from 'redux-promise';
+// Thunk and promise are middlewares for redux
+import thunk from "redux-thunk"
+import promise from 'redux-promise'
 
-const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
-export const store = createStoreWithMiddleware(reducers);
+// Middleware is used in redux to make reducers return new global app states
+const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore)
+export const store = createStoreWithMiddleware(reducers)
 
 ReactDOM.render(
   <Provider store={store}>
